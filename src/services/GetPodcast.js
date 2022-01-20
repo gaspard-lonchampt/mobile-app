9a808197ad1120722c963eb625d7c33d605c9cc0
gaspard-lonchampt <gaspard.lonchampt@laplateforme.io>
2022-01-20 16:31:34 +0100

    [Features] - Merge Transistor API branch
    - add search component for podcast on transistor;
    - add fetch component to get podcast on transistor;
    - add dashboard component to see informations;
    - add global navigation architecture.

---
+++ src/services/GetPodcast.js
@@ -0,0 +1,66 @@
+import { View, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from "react"
+import { PodcastEpisodes } from '../screens/PodcastEpisodes';
+import { PodcastShows } from '../screens/PodcastShows';
+
+function GetPodcast(props) {
+
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+    };
+
+    const host = 'https://proxy-grenouille.herokuapp.com/';
+    const type = props.route.params.route;
+    // const type = 'shows';
+    // console.log(type); 
+    // const host = 'http://localhost:3001/';
+    // const host = 'https://api.transistor.fm/v1/';
+
+    let podcast = type.toLowerCase().trim();
+    const URL = host.concat(podcast);
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    var content;
+
+    useEffect(() => {
+        fetch(URL, requestOptions)
+        // setLoading(true)
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log('getting data from Transistor', responseJson)
+                setData(responseJson);
+                setLoading(false);
+            })
+            .catch(error => console.log('error', error));
+            // setLoading(false)
+    }, [])
+
+    if(loading) {
+        content = <ActivityIndicator /> 
+    } else {
+        if (type == 'shows') {
+            return (
+                <PodcastShows data={data} />
+            )
+        } else if (type == 'episodes') {
+            return <PodcastEpisodes data={data} />
+        } else {
+            alert("ceci n'existe pas");
+        }
+    }
+
+    console.log(content);
+    return (
+        <View>
+            {content}
+        </View>
+    )
+};
+
+export default GetPodcast 
