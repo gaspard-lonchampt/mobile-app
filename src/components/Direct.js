93a25515cbd33831cf2ba24d0fe71f646088f988
gaspard-lonchampt <gaspard.lonchampt@laplateforme.io>
2022-01-20 17:22:37 +0100

    [Features] - Transistor API
    - add search component for podcast on transistor;
    - add fetch component to get podcast on transistor;
    - add dashboard component to see informations;
    - add global navigation architecture.

---
+++ src/components/Direct.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function Direct() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Direct!</Text>
+        </View>
+    );
+}
+
+export default Direct
