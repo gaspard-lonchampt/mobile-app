c2c8f1ced59e31d21db69a7e1778990d7a7f1610
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2022-02-14 18:19:05 +0100

    base loop ok

---
+++ src/programs/index.js
@@ -0,0 +1,69 @@
+import { Image, ScrollView, ActivityIndicator,View,Text, FlatList  } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+
+
+
+const Programs = () => {
+  // const [data, setData] = useState({});
+  const [loader, setLoader] = useState(false)
+  const [category, setCategory] = useState({})
+
+  const myFetch = async () => {
+    setLoader(true)
+     try {
+      const response = await fetch('http://127.0.0.1:8000/categories/');
+      const json = await response.json();
+  
+      setCategory(json['hydra:member'])
+      setLoader(false)
+     
+
+    } catch (error) {
+      console.error(error);
+      setLoader(false)
+    } 
+  }
+
+
+  const loop = (data) =>{
+
+    let array = []
+    for (let i =0; i < data.length; i++){
+       array.push(data[i].name)
+    }
+
+    return array.map(cat => <Text key={cat}>{cat}</Text>)
+
+  }
+
+  useEffect(()=>{
+    myFetch()
+  },[])
+
+
+
+
+
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+
+      {/* {console.log(category.length)} */}
+
+    {loader ? <ActivityIndicator/> :   <> {loop(category)} </>}
+
+
+
+
+
+
+    </View>
+  )
+
+
+}
+
+
+
+export default Programs;
