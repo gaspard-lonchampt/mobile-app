b7d1e580980e811bf874ea445d95681702850e30
Baptiste <baptiste.gauthier@synergiefamily.com>
2022-02-16 15:30:37 +0100

    [features] Register
    - Vue Registration ok
    - Registration POST with API ok
    - Error message and constrainst to Login / Registration ok

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/components/Login';
+import Register from './src/components/Register' ; 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
+      {/* <Login /> */}
+      <Register />
       <StatusBar style="auto" />
     </View>
   );
